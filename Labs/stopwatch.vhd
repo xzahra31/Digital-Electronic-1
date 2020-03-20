9c211555df705a1be8472f921b53dce7f00ff811
xzahra31 <60693385+xzahra31@users.noreply.github.com>
2020-03-20 23:43:23 +0100

    Add files via upload

---
+++ Labs/stopwatch.vhd
@@ -0,0 +1,76 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity stopwatch is
+
+port(
+    clk_i       : in std_logic;
+    ce_100Hz_i  : in std_logic;
+    srst_n_i    : in std_logic;
+    cnt_en_i    : in std_logic; 
+  
+    sec_h_o     : out unsigned (3 downto 0);
+    sec_l_o     : out unsigned (3 downto 0);
+    hth_h_o     : out unsigned (3 downto 0);
+    hth_l_o     : out unsigned (3 downto 0)
+	);
+end entity stopwatch;
+
+architecture Behavioral of stopwatch is
+    
+	signal cnt_sec_h_o  : unsigned (3 downto 0) := (others => '0');
+	signal cnt_sec_l_o  : unsigned (3 downto 0) := (others => '0');
+	signal cnt_hth_h_o  : unsigned (3 downto 0) := (others => '0');
+	signal cnt_hth_l_o  : unsigned (3 downto 0) := (others => '0');
+   signal cnt_en		  : std_logic;
+
+begin
+     
+    p_assign: process(clk_i)
+    begin
+	 
+    	if rising_edge(clk_i) then 
+				hth_l_o <= unsigned(cnt_hth_l_o);
+            hth_h_o <= unsigned(cnt_hth_h_o);
+            sec_l_o <= unsigned(cnt_sec_l_o);
+            sec_h_o <= unsigned(cnt_sec_h_o);
+            if cnt_en_i = '1' then
+            	cnt_en 	<=	'0';
+            else
+            	cnt_en 	<=	'1';
+            end if;
+        end if;
+		  
+      end process; 
+            
+    p_cnt: process(clk_i, srst_n_i, cnt_en, cnt_sec_h_o, cnt_sec_l_o, cnt_hth_h_o, cnt_hth_l_o)
+	 
+    begin 
+	 
+		if rising_edge(clk_i) then    	    
+				if srst_n_i = '0' or (cnt_hth_l_o = "1001" and cnt_hth_h_o = "1001" and cnt_sec_l_o = "1001" and cnt_sec_h_o = "0101") then
+						cnt_sec_h_o <= (others => '0');  
+						cnt_sec_l_o <= (others => '0');
+						cnt_hth_h_o <= (others => '0');
+						cnt_hth_l_o <= (others => '0');
+					elsif cnt_en = '1' and ce_100Hz_i = '1' then
+							cnt_hth_l_o <= cnt_hth_l_o + 1;
+						if cnt_hth_l_o = "1001" then
+							cnt_hth_l_o <= (others => '0');
+							cnt_hth_h_o <= cnt_hth_h_o + 1;
+							if cnt_hth_h_o = "1001" then
+								cnt_hth_h_o <= (others => '0');
+								cnt_sec_l_o <= cnt_sec_l_o + 1;
+								if cnt_sec_l_o = "1001" then
+									cnt_sec_l_o <= (others => '0');
+									cnt_sec_h_o <= cnt_sec_h_o + 1;
+                        end if;
+                    end if;
+                end if;
+            end if;
+        end if;
+		  
+    end process; 
+	 
+end architecture Behavioral;
---
+++ Labs/testbench.vhd
@@ -0,0 +1,65 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity testbench is
+--empty
+end testbench;
+
+architecture tb of testbench is
+-- DUT component
+component driver_7seg is
+port(
+	clk_i     : in  std_logic;
+    srst_n_i : in  std_logic;  
+    cnt_en_i : in  std_logic;	
+    seg_o    : out unsigned(7-1 downto 0);
+    dig_o    : out unsigned(4-1 downto 0)
+);
+end component;
+
+signal clk_in   	: 	std_logic := '0';
+signal srst_n_in 	:  std_logic := '0';   
+signal cnt_en_in	:  std_logic := '0';
+signal seg_out   	:  unsigned(7-1 downto 0);
+signal dig_out    :  unsigned(4-1 downto 0);
+
+
+BEGIN
+	UUT: driver_7seg port map
+							(
+								clk_i => clk_in, 
+								srst_n_i => srst_n_in, 
+								cnt_en_i => cnt_en_in,
+      						seg_o => seg_out, 
+      						dig_o => dig_out
+    						);
+
+
+	Clk_gen: process	-- NEW
+  	begin
+    	while Now < 1000 NS loop		
+      		clk_in <= '0';
+      		wait for 0.5 NS;
+      		clk_in <= '1';
+      		wait for 0.5 NS;
+    	end loop;
+    	wait;
+  	end process Clk_gen;
+   
+   -- Stimulus process
+   stim_proc: process
+   begin		
+ 
+      srst_n_in <= '1';
+      wait until rising_edge(clk_in);	
+      wait until rising_edge(clk_in);	
+      srst_n_in <= '0';
+      wait until rising_edge(clk_in);	
+      wait until rising_edge(clk_in);	
+      wait until rising_edge(clk_in);	
+      srst_n_in <= '1';
+      
+      wait;
+   end process;
+end tb;
